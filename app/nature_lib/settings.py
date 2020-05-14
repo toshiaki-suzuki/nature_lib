1d9aa531772b9a6f46c46802165f46fb5774cad3
toshiakisuzuki <flaflabell@gmail.com>
2020-05-14 20:26:36 +0900

    create nature_lib app

--- app/nature_lib/settings.py
+++ app/nature_lib/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nature_lib_app',
 ]
 
 MIDDLEWARE = [
---
+++ app/nature_lib_app/urls.py
@@ -0,0 +1,6 @@
+from .views import IndexView
+from django.urls import path
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+]
